3a3602b0f9c98eae2445bd636a9f267b7b288998
mukassar <shaikhmukassar@gmail.com>
2022-07-08 09:47:01 +0530

    Todo list component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { Home } from './Components/Home';
-import TextForm from './Components/TextForm';
+import { StrictMode } from "react";
+// import Fetch from "./Components/Fetch";
+// import BesicForm from './Components/BasicForm';
+import { Home } from "./Components/Home";
+
+import TextForm from "./Components/TextForm";
+import Todo from "./Components/Todo";
 
 function App() {
-  
-  
   return (
     <>
-      <Home title="TextUtils" AboutText="About Us"/>
+      <StrictMode>
+        <Home title="TextUtils" AboutText="About Us" />
 
-      <div className="container my-3" >
-
-      <TextForm heading="Enter the text to analyze"/>
-
-      </div>
+        <div className="container my-3">
+          <TextForm heading="Enter the text to analyze" />
+          <Todo />
+          {/* <BesicForm/>   */}
+          
+        </div>
+        {/* <Fetch /> */}
+      </StrictMode>
     </>
   );
 }
